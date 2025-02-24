e0752b827b79e607109a3cca4150e93a526a5d7f
quelsor <160043804+quelsor@users.noreply.github.com>
2025-02-25 00:56:06 +0200

    Add images, information about team

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 const members = [
-  { id: 1, name: "Ім'я Учасника 1", description: "Опис учасника 1" },
-  { id: 2, name: "Ім'я Учасника 2", description: "Опис учасника 2" },
-  { id: 3, name: "Ім'я Учасника 3", description: "Опис учасника 3" }
+  { id: 1, name: "Тараненко Артем", description: "Опис учасника 1" , img: "/images/gif.gif"},
+  { id: 2, name: "Навроцький Максим", description: "Опис учасника 2" , img: "/images/gif_max.gif"},
+  { id: 3, name: "Суліма Лілія", description: "Опис учасника 3" , img: "/images/gif_liliia.gif"},
+  { id: 4, name: "Долинко Олександр", description: "Опис учасника 4" , img: "/images/gif_sasha.gif"}
 ];
 
 /* Сторінка учасника */
