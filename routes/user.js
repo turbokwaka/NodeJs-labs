b45041c3de5203b685ecb8482f4f7c5d7818f432
Max <navrotskyimaxym@gmail.com>
2025-03-08 23:24:10 +0200

    Оновив стилі та оновлення у файлах Pug

--- routes/user.js
+++ routes/user.js
@@ -2,10 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 const users = [
-  { id: 1, name: "Тараненко Артем", description: "Опис учасника 1" , img: "/images/gif.gif"},
-  { id: 2, name: "Навроцький Максим", description: "Опис учасника 2" , img: "/images/gif_max.gif"},
-  { id: 3, name: "Суліма Лілія", description: "Опис учасника 3" , img: "/images/gif_liliia.gif"},
-  { id: 4, name: "Долинко Олександр", description: "Опис учасника 4" , img: "/images/gif_sasha.gif"}
+  { id: 1, name: "Тараненко Артем", description: "Работнік  Binotel LLC. Дуже потужний працівник посада:я не помню!" , img: "/images/gif.gif"},
+  { id: 2, name: "Навроцький Максим", description: "Работнік City24. Майже потужний аналітік віділу фінансуванні і підтримки!" , img: "/images/gif_max.gif"},
+  { id: 3, name: "Суліма Лілія", description: "Работнік: Сижу дома мені добре!" , img: "/images/gif_liliia.gif"},
+  { id: 4, name: "Долинко Олександр", description: "Работнік: Стіпуха кормить!!!" , img: "/images/gif_sasha.gif"}
 ];
 
 /* Сторінка учасника */
