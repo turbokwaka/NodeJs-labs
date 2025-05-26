cf77860d84c5efb2fe25d0083e409f3cffabcddc
quelsor <160043804+quelsor@users.noreply.github.com>
2025-05-26 17:20:36 +0300

    додала форму login

--- public/stylesheets/main-page-script.js
+++ public/stylesheets/main-page-script.js
@@ -12,3 +12,37 @@ btnLeft.addEventListener('click', () => {
     carousel.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
 });
 
+const loginBtn = document.querySelector('.login-btn');
+const loginForm = document.querySelector('.login-form');
+const submitBtn = document.querySelector('.login_submit');
+
+loginBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    loginForm.classList.toggle('show');})
+
+document.addEventListener('click', (e) => {
+    const clickedInsideForm = loginForm.contains(e.target);
+    const clickedLoginBtn = loginBtn.contains(e.target);
+
+    if (!clickedInsideForm && !clickedLoginBtn) {
+        loginForm.classList.remove('show');
+    }
+})
+
+submitBtn.addEventListener('click', () => {
+    showNotification('You successfully LogIn');
+});
+
+const notification = document.getElementById('notification');
+
+function showNotification(message) {
+    notification.textContent = message;
+    notification.classList.remove('hidden');
+
+    setTimeout(() => {
+        notification.classList.add('hidden');
+    }, 3000);
+}
+
+
+
